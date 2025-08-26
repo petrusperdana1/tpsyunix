7c09089411791722d2ba257c610ec095fb1b5cec
petrusperdana1 <55475333+petrusperdana1@users.noreply.github.com>
2025-08-26 09:01:57 +0700

    Create notif.js

---
+++ notif.js
@@ -0,0 +1,11 @@
+<div class="notificationModal-content">
+    <i class="fa fa-times close-icon" id="notification-close"></i>
+    <h2>Informasi</h2>
+    <p>DISKON 10% DENGAN KODE: <b>PAYDAYAGUSTUS10</b></p>
+    <p>
+    </p>
+    <p>
+    </p>
+    <p>Laporan services sudah tersedia di channel Telegram: <b>https://bit.ly/groupychannel</b></p>
+    <button class="ud-main-btn" id="notification-ok">OK</button>
+</div>
