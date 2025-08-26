3bc12267739d0835af6e05bb271bc87fb37bed19
petrusperdana1 <55475333+petrusperdana1@users.noreply.github.com>
2025-08-26 11:25:06 +0700

    Update notif.js

--- notif.js
+++ notif.js
@@ -1,5 +1,5 @@
 {
-  "enabled": true,
+  "enabled": false,
   "title": "Informasi",
   "discountLine": "KEMUNGKINGKAN DALAM SATU MINGGU INI AKAN ADA <b>UPDATE</b>",
   "telegramLine": "Sehingga menyebabkan error jadi mohon bersabar ya 😊🙏: <b>https://petrussiahaan.blogspot.com/</b>"
