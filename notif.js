183c8d9fa48f549304864b57312c6036453f4da7
petrusperdana1 <55475333+petrusperdana1@users.noreply.github.com>
2025-08-26 09:41:00 +0700

    Update notif.js

--- notif.js
+++ notif.js
@@ -1,6 +1,6 @@
 {
   "enabled": true,
   "title": "Informasi",
-  "discountLine": "DISKON 10% DENGAN KODE: <b>PAYDAYAGUSTUS10</b>",
-  "telegramLine": "Laporan services sudah tersedia di channel Telegram: <b>https://bit.ly/groupychannel</b>"
+  "discountLine": "DALAM SATU MINGGU INI AKAN ADA <b>UPDATE</b>",
+  "telegramLine": "Sehingga menyebabkan error jadi mohon bersabar ya 😊🙏: <b>https://petrussiahaan.blogspot.com/</b>"
 }
