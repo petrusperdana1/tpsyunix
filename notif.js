8d9eace47adce58fc1f8cf6f8063f22d810838fa
petrusperdana1 <55475333+petrusperdana1@users.noreply.github.com>
2025-08-28 13:02:06 +0700

    Update notif.js

--- notif.js
+++ notif.js
@@ -1,6 +1,7 @@
 {
   "enabled": true,
-  "title": "Informasi",
-  "discountLine": "<b>ChatGPT under maintenance, please wait patiently.</b>",
-  "telegramLine": "Sehingga menyebabkan error jadi mohon bersabar ya 😊🙏: <b>https://petrussiahaan.blogspot.com/</b>"
-}
+  "id": "info-update-agustus-2025",
+  "title": "Informasi Update",
+  "discountLine": "<b>ChatGPT under maintenance, please wait patiently.</b><br>DALAM SATU MINGGU INI AKAN ADA <b>UPDATE</b>",
+  "telegramLine": "Sehingga menyebabkan error jadi mohon bersabar ya 😊🙏. : <b>https://petrussiahaan.blogspot.com/</b>"
+ }
