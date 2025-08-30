ec7b81af7747cae1ad7502857bb25712cdd11111
petrusperdana1 <55475333+petrusperdana1@users.noreply.github.com>
2025-08-30 14:46:51 +0700

    Update notif.js

--- notif.js
+++ notif.js
@@ -3,5 +3,5 @@
   "id": "info2-update-agustus-2025",
   "title": "Informasi Update",
   "discountLine": "Karena masa depan sungguh ada, dan harapanmu tidak akan hilang. <b>Amsal 23 : 18</b>",
-  "telegramLine": "😊🙏. : <b>https://petrussiahaan.blogspot.com/</b>"
+  "telegramLine": "😊🙏 <b>petrussiahaan.blogspot.com</b>"
  }
