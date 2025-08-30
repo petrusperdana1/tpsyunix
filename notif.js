fad3f761f37a6e7582b67751fc8e374c1fd9869d
petrusperdana1 <55475333+petrusperdana1@users.noreply.github.com>
2025-08-30 19:11:07 +0700

    Update notif.js

--- notif.js
+++ notif.js
@@ -1,7 +1,29 @@
 {
-  "enabled": true,
-  "id": "info2-update-agustus-2025",
-  "title": "Informasi Update",
-  "discountLine": "Karena masa depan sungguh ada, dan harapanmu tidak akan hilang. <b>Amsal 23 : 18</b> 😊🙏",
-  "telegramLine": "<b>petrussiahaan.blogspot.com</b>"
- }
+  "announcement": {
+    "enabled": true,
+    "id": "info2-update-agustus-2025",
+    "title": "Informasi Update",
+    "lines": [
+      "Karena masa depan sungguh ada, dan harapanmu tidak akan hilang. <b>Amsal 23 : 18</b> 😊🙏",
+      "<b>petrussiahaan.blogspot.com</b>"
+    ]
+  },
+  "folderInfo": {
+    "chatgpt-group-id": {
+      "title": "ChatGPT",
+      "content": "<p><img src=\"https://us1.discourse-cdn.com/openai1/original/4X/7/0/1/701493b167007f5ab689eda8f388260e7390abc1.png\" width=\"90%\"></p><p>Jika kamu mengalami pesan error: <b>\"Something went wrong while generating the response\"</b>, cukup hapus cookie dan cache pada browser yang kamu gunakan.</p><p>Jika kamu mengalami pesan error: <b>\"Our system have detected unusual activity from your system\"</b>, silahkan gunakan akun lain atau gunakan alternatif seperti <b>Sider/NoteGPT/You/Merlin</b>.</p><p>Jika model o1 sedang limit, kamu tetap dapat menggunakan model 4o.</p>"
+    },
+    "freepik-group-id": {
+      "title": "Informasi Freepik",
+      "content": "<p>Jika kamu ingin mengunduh file <b>.mv</b> atau <b>.mp4</b>, gunakan <b>Freepik 2-3</b></p><p><b>Freepik 1</b> (Server 1-10 digunakan untuk mengunduh tipe file jpg, png, tidak untuk mengunduh icon / video)</p>"
+    },
+    "perplexity-group-id": {
+      "title": "Info Perplexity",
+      "content": "<p>Jika Perpexity mengalami error <b>Lost Connection</b>, kamu masih dapat menggunakannya.</p>"
+    },
+    "prime-video-group-id": {
+      "title": "Info Prime Video",
+      "content": "<p>Jika Prime Video meminta PIN, masukan <b>12345</b></p>"
+    }
+  }
+}
