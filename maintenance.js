5e3ddd121853a1c2eac8d61e12db0260aaa3caba
petrusperdana1 <55475333+petrusperdana1@users.noreply.github.com>
2025-09-17 19:12:00 +0700

    Update maintenance.js

--- maintenance.js
+++ maintenance.js
@@ -1,11 +1,11 @@
 {
     "maintenance_enabled": true,
-    "maintenance_time": "19.33",
+    "maintenance_time": "19.22",
     "maintenance_duration_minutes": 1,
     "maintenance_active_message": "Maaf tunggu sebentar, sedang maintenance. Hanya satu menit...",
 
     "warning_enabled": true,
-    "warning_minutes_before": 30,
+    "warning_minutes_before": 10,
     "warning_title": "Informasi Maintenance",
     "warning_message": "Sesi akan di-reset untuk pemeliharaan rutin."
 }
