248b998dede6268f770442d199dffde6cac9ebf9
petrusperdana1 <55475333+petrusperdana1@users.noreply.github.com>
2025-09-17 19:12:10 +0700

    Update maintenance.js

--- maintenance.js
+++ maintenance.js
@@ -1,6 +1,6 @@
 {
     "maintenance_enabled": true,
-    "maintenance_time": "19.22",
+    "maintenance_time": "19.23",
     "maintenance_duration_minutes": 1,
     "maintenance_active_message": "Maaf tunggu sebentar, sedang maintenance. Hanya satu menit...",
 
