30aaf5c96550debea8c5c5dc0b25a5124b9ef859
petrusperdana1 <55475333+petrusperdana1@users.noreply.github.com>
2025-09-19 22:37:52 +0700

    Update aturhonma.js

--- aturhonma.js
+++ aturhonma.js
@@ -44,7 +44,7 @@ const gracelyConfig = {
   "maintenanceMode": {
     "enabled": true,
     "scheduleType": "daily",
-    "dailyTime": "22:39:00",
+    "dailyTime": "22:45:00",
     "specificDateTime": "2025-12-31T23:00:00",
     "durationMinutes": 2,
     "countdownMinutes": 10,
