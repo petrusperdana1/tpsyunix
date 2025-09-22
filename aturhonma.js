de12cf0bcff403d7c1cc53699223e9bc999bedf9
petrusperdana1 <55475333+petrusperdana1@users.noreply.github.com>
2025-09-22 19:58:28 +0700

    Update aturhonma.js

--- aturhonma.js
+++ aturhonma.js
@@ -10,7 +10,7 @@ const gracelyConfig = {
     "onLogoutBlock": "https://petrussiahaan.blogspot.com/p/ruang-syahdu.html"
   },
   "dataSources": {
-    "services": "https://petrusperdana1.github.io/tpsyunix/databaru.js"
+    "services": "https://petrusperdana1.github.io/tpsyunix/databaru1.js"
   },
   "notifications": {
     "announcement": {
