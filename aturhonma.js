85730b8e5c2eadcc537f05944966e1305bf68caf
petrusperdana1 <55475333+petrusperdana1@users.noreply.github.com>
2025-10-09 08:19:40 +0700

    Update aturhonma.js

--- aturhonma.js
+++ aturhonma.js
@@ -44,9 +44,9 @@ const gracelyConfig = {
   "maintenanceMode": {
     "enabled": true,
     "scheduleType": "daily",
-    "dailyTime": "21:25:00",
+    "dailyTime": "08:22:00",
     "specificDateTime": "2025-12-31T23:00:00",
-    "durationMinutes": 1,
+    "durationMinutes": 3,
     "countdownMinutes": 10,
     "warningTitle": "Peringatan Maintenance",
     "warningMessage": "Layanan akan memasuki mode perbaikan dalam:",
