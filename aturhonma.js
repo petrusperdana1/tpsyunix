91edac8c2f3e51399fdbd04b81a335d3f342d506
petrusperdana1 <55475333+petrusperdana1@users.noreply.github.com>
2025-10-20 19:38:21 +0700

    Update aturhonma.js

--- aturhonma.js
+++ aturhonma.js
@@ -33,7 +33,7 @@ const gracelyConfig = {
       },
       "canva-group-id": {
         "title": "Informasi",
-        "content": "<p>Silakan undang email pribadi kamu untuk bergabung dengan team Canva Groupy. Jika sudah penuh, mohon laporkan melalui server Discord atau channel Telegram Groupy.</p>"
+        "content": "<p>Silakan undang email pribadi kamu untuk bergabung dengan team Canva. Jika sudah penuh, mohon laporkan melalui server Discord atau channel Telegram Groupy.</p>"
       },
       "perplexity-group-id": {
         "title": "Info Perplexity",
